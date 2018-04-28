9dc324db2ebc2b4a08107d7ab4ca51546057f04b
Antti Niemi <antti.niemi@kuuasema.com>
2018-04-28 16:13:15 +0300

    Initial project

---
+++ Assets/SurfController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurfPartSpecs {
+
+}
+
+public class SurfController : MonoBehaviour {
+    public GameObject surfPartPrefab;
+	private List<GameObject> surfPlanes;
+    void Start() {
+        GenerateSurfPart();
+    }
+
+    void Update() {
+
+    }
+
+    private void GenerateSurfPart(SurfPartSpecs specs = null) {
+        if (specs == null) {
+
+            surfPlanes.Add(Instantiate(surfPartPrefab, new Vector3(0, 0, 0), Quaternion.identity));
+        }
+    }
